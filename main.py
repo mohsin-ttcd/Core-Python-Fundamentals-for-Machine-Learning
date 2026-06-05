f89164a5b99bd4eb654c23694a2e04f80e5bb201
agentMohsin <mohsin.ttcdl@gmail.com>
2026-06-05 23:27:03 +0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+raw_data = "  mission:apollo11; mission_code:A11; status:success"
+
+clean_raw_data = raw_data.strip().upper()
+
+process_raw_data = clean_raw_data.split(";")
+
+final_formated_data = process_raw_data[1].strip()
+
+print(f"Cleaned Raw Data: {clean_raw_data}")
+print(f"Formated Data: {final_formated_data}")
